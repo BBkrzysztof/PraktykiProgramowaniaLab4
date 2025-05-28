e8348ad023dd2a78218e928a9f615b6939773355
Krzysztof Brylski <krzysztof.brylski@codewave.pl>
2025-05-28 15:57:07 +0200

    fixed formating

--- utils.py
+++ utils.py
@@ -1,29 +1,31 @@
 """
-    simple calculator
+simple calculator
 """
+
 
 def add(a: int, b: int) -> int:
     """
-        function for adding two numbers
+    function for adding two numbers
     """
     return a + b
 
 
 def subtract(a: int, b: int) -> int:
     """
-        function for substracting two numbers
+    function for substracting two numbers
     """
     return a - b
 
 
 def multiply(a: int, b: int) -> int:
     """
-        function for mutliplying two numbers
+    function for mutliplying two numbers
     """
     return a * b
 
+
 def divide(a: int, b: int) -> float:
     """
-        function for dividing two numbers
+    function for dividing two numbers
     """
     return a / b
